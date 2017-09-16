9a6b6b7c45f15d43362ac4258bfb76428b4d55c0
Neijwiert <guidoknapen@gmail.com>
2017-09-16 17:29:11 +0200

    Reversed M01_Barn_Truck_JDG

---
+++ Source/Single Player Scripts/M01_Barn_Truck_JDG.cpp
@@ -0,0 +1,28 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "General.h"
+#include "M01_Barn_Truck_JDG.h"
+
+void M01_Barn_Truck_JDG::Killed(GameObject *obj, GameObject *killer)
+{
+	Vector3 pos = Commands->Get_Position(obj);
+	Commands->Create_Explosion("Explosion_Vehicle", pos, NULL);
+}
+
+ScriptRegistrant<M01_Barn_Truck_JDG> M01_Barn_Truck_JDGRegistrant("M01_Barn_Truck_JDG", "");
