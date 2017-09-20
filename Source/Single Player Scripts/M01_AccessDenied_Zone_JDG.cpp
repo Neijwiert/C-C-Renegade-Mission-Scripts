b93844ff5dafce377df2f561f1d9e0b57ca5584b
Neijwiert <guidoknapen@gmail.com>
2017-09-20 12:16:27 +0200

    Reversed M01_AccessDenied_Zone_JDG

---
+++ Source/Single Player Scripts/M01_AccessDenied_Zone_JDG.cpp
@@ -0,0 +1,33 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "General.h"
+#include "M01_AccessDenied_Zone_JDG.h"
+
+void M01_AccessDenied_Zone_JDG::Entered(GameObject *obj, GameObject *enterer)
+{
+	Vector3 pos = Commands->Get_Position(obj);
+	if (enterer == Commands->Get_A_Star(pos))
+	{
+		int conversationId = Commands->Create_Conversation("M01_Lockdown_Conversation", 100, 1000.0f, true); // Access denied.
+		Commands->Join_Conversation(NULL, conversationId, false, false, true);
+		Commands->Start_Conversation(conversationId, conversationId);
+	}
+}
+
+ScriptRegistrant<M01_AccessDenied_Zone_JDG> M01_AccessDenied_Zone_JDGRegistrant("M01_AccessDenied_Zone_JDG", "");
