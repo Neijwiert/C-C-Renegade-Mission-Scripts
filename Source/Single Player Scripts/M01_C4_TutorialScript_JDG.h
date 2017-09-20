c34c0af37ef3233dbb68e97f959d7e9d311c01b6
Neijwiert <guidoknapen@gmail.com>
2017-09-20 22:22:34 +0200

    Reversed M01_C4_TutorialScript_JDG

---
+++ Source/Single Player Scripts/M01_C4_TutorialScript_JDG.h
@@ -0,0 +1,30 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <scripts.h>
+
+/*
+M01 -> 1200196 117621 117617 117421
+*/
+class M01_C4_TutorialScript_JDG : public ScriptImpClass
+{
+	private:
+		virtual void Custom(GameObject *obj, int type, int param, GameObject *sender);
+};
