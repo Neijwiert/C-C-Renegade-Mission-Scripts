a5c773a86692ffdd1a91945259a54135815b9271
Neijwiert <guidoknapen@gmail.com>
2017-09-23 14:15:09 +0200

    Reversed M01_MiniGunner_Point_Guard_JDG

---
+++ Source/Single Player Scripts/M01_MiniGunner_Point_Guard_JDG.h
@@ -0,0 +1,29 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <scripts.h>
+
+/*
+M01 -> 102185 101939 101686 101685 101175 101173 101165 101164 101163 101162 100893 100894 100896 100898 100899 101121 101125 101126 101321 101336 101337 103340 103341 103342
+*/
+class M01_MiniGunner_Point_Guard_JDG : public ScriptImpClass
+{
+	// This script literally has nothing
+};
