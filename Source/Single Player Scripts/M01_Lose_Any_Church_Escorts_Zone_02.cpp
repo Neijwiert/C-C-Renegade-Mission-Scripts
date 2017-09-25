2545aa923e1d4a8394decf5350b31188b4e0fe45
Neijwiert <guidoknapen@gmail.com>
2017-09-25 20:31:55 +0200

    Reversed M01_Lose_Any_Church_Escorts_Zone_02

---
+++ Source/Single Player Scripts/M01_Lose_Any_Church_Escorts_Zone_02.cpp
@@ -0,0 +1,35 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "General.h"
+#include "M01_Lose_Any_Church_Escorts_Zone_02.h"
+
+/*
+M01 -> 100883
+*/
+void M01_Lose_Any_Church_Escorts_Zone_02::Entered(GameObject *obj, GameObject *enterer)
+{
+	Vector3 pos = Commands->Get_Position(obj);
+	if (enterer == Commands->Get_A_Star(pos))
+	{
+		GameObject *M01MissionControllerJDGObj = Commands->Find_Object(100376);
+		Commands->Send_Custom_Event(obj, M01MissionControllerJDGObj, 0, 150, 0.0f);
+	}
+}
+
+ScriptRegistrant<M01_Lose_Any_Church_Escorts_Zone_02> M01_Lose_Any_Church_Escorts_Zone_02Registrant("M01_Lose_Any_Church_Escorts_Zone_02", "");
