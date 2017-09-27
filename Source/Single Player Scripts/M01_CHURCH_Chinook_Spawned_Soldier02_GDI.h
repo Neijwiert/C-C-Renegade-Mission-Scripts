b68877046a816b35a427c35bb02c276b3af8ba90
Neijwiert <guidoknapen@gmail.com>
2017-09-27 14:48:03 +0200

    Reversed M01_CHURCH_Chinook_Spawned_Soldier02_GDI

---
+++ Source/Single Player Scripts/M01_CHURCH_Chinook_Spawned_Soldier02_GDI.h
@@ -0,0 +1,39 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <scripts.h>
+
+class M01_CHURCH_Chinook_Spawned_Soldier02_GDI : public ScriptImpClass
+{
+	public:
+		virtual	void Register_Auto_Save_Variables();
+
+	private:
+		virtual void Created(GameObject *obj);
+		virtual void Killed(GameObject *obj, GameObject *killer);
+		virtual void Damaged(GameObject *obj, GameObject *damager, float amount);
+		virtual void Custom(GameObject *obj, int type, int param, GameObject *sender);
+		virtual void Enemy_Seen(GameObject *obj, GameObject *enemy);
+		virtual void Action_Complete(GameObject *obj, int action_id, ActionCompleteReason complete_reason);
+		virtual void Animation_Complete(GameObject *obj, const char *animation_name);
+		virtual void Poked(GameObject *obj, GameObject *poker);
+
+		int field_1C; // Yes, this is an int
+};
