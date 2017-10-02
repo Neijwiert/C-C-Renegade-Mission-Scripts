ef7d31d99b9f424bc24758996734c3eb28ab7de7
Neijwiert <guidoknapen@gmail.com>
2017-10-02 23:01:54 +0200

    Reversed M01_Right_Interrogation_Room_Enter_Zone_JDG

---
+++ Source/Single Player Scripts/M01_Right_Interrogation_Room_Enter_Zone_JDG.cpp
@@ -0,0 +1,50 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "General.h"
+#include "M01_Right_Interrogation_Room_Enter_Zone_JDG.h"
+
+/*
+M01 -> 101355
+*/
+void M01_Right_Interrogation_Room_Enter_Zone_JDG::Entered(GameObject *obj, GameObject *enterer)
+{
+	Vector3 pos = Commands->Get_Position(obj);
+	if (enterer == Commands->Get_A_Star(pos))
+	{
+		GameObject *honInterrogationRoomSupriseGuyObj = Commands->Find_Object(101357);
+		if (honInterrogationRoomSupriseGuyObj)
+		{
+			Commands->Send_Custom_Event(obj, honInterrogationRoomSupriseGuyObj, 0, 156, 0.0f);
+		}
+
+		GameObject *M01LeftInterrogationRoomEnterZoneJDGObj = Commands->Find_Object(101354);
+		if (M01LeftInterrogationRoomEnterZoneJDGObj)
+		{
+			Commands->Destroy_Object(M01LeftInterrogationRoomEnterZoneJDGObj);
+		}
+
+		GameObject *M01RightInterrogationRoomEnterZoneJDGObj = Commands->Find_Object(101355);
+		if (M01RightInterrogationRoomEnterZoneJDGObj)
+		{
+			Commands->Destroy_Object(M01RightInterrogationRoomEnterZoneJDGObj);
+		}
+	}
+}
+
+ScriptRegistrant<M01_Right_Interrogation_Room_Enter_Zone_JDG> M01_Right_Interrogation_Room_Enter_Zone_JDGRegistrant("M01_Right_Interrogation_Room_Enter_Zone_JDG", "");
