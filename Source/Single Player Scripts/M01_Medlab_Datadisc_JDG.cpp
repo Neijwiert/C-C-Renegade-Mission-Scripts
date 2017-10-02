0a99b12f5c4e602180b3d791dd1cb2c68fbffac5
Neijwiert <guidoknapen@gmail.com>
2017-10-02 23:38:24 +0200

    Reversed M01_Medlab_Datadisc_JDG

---
+++ Source/Single Player Scripts/M01_Medlab_Datadisc_JDG.cpp
@@ -0,0 +1,41 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "General.h"
+#include "M01_Medlab_Datadisc_JDG.h"
+
+/*
+M01 -> 125064
+*/
+void M01_Medlab_Datadisc_JDG::Custom(GameObject *obj, int type, int param, GameObject *sender)
+{
+	if (type == CUSTOM_EVENT_POWERUP_GRANTED)
+	{
+		Commands->Set_HUD_Help_Text(7587, Vector3(0.196f, 0.882f, 0.196f)); // Data Link Updated - Satellite Image Enhanced\n
+
+		Commands->Reveal_Map();
+
+		GameObject *M01DataDiscTextControllerJDGObj = Commands->Find_Object(117188);
+		if (M01DataDiscTextControllerJDGObj)
+		{
+			Commands->Send_Custom_Event(obj, M01DataDiscTextControllerJDGObj, 0, 10, 2.0f);
+		}
+	}
+}
+
+ScriptRegistrant<M01_Medlab_Datadisc_JDG> M01_Medlab_Datadisc_JDGRegistrant("M01_Medlab_Datadisc_JDG", "");
