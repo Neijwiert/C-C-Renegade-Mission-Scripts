7c919761fd63d1faa69b2f17d25fc88dffd13dc6
Neijwiert <guidoknapen@gmail.com>
2017-10-05 00:55:37 +0200

    Reversed M01_COMM_Commander_Guy (All scripts for M01)

---
+++ Source/Single Player Scripts/M01_COMM_Commander_Guy.cpp
@@ -0,0 +1,29 @@
+/*
+* A Command & Conquer: Renegade SSGM Plugin, containing all the single player mission scripts
+* Copyright(C) 2017  Neijwiert
+*
+* This program is free software : you can redistribute it and / or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "General.h"
+#include "M01_COMM_Commander_Guy.h"
+
+// This script is never used
+void M01_COMM_Commander_Guy::Killed(GameObject *obj, GameObject *killer)
+{
+	GameObject *M01MissionControllerJDGObj = Commands->Find_Object(100376);
+	Commands->Send_Custom_Event(obj, M01MissionControllerJDGObj, 0, 178, 0.0f);
+}
+
+ScriptRegistrant<M01_COMM_Commander_Guy> M01_COMM_Commander_GuyRegistrant("M01_COMM_Commander_Guy", "");
